f9ad05ce7d4d37c0d3b46e1a54dc968d3a31410a
guo_x <guo_x0315@163.com>
2025-04-15 21:41:09 +0800

    新增：添加新的 Java 类文件- 新增 ArrayMulti_283 类，实现力扣283题 - 新增 HIndex_274 类，实现 H指数计算功能 - 新增 JumpGame02_45 类，实现跳跃游戏 2 功能

--- src/test/java/Hot150Test.java
+++ src/test/java/Hot150Test.java
@@ -9,7 +9,18 @@ import java.util.Arrays;
  * @Description:
  */
 
-public class TestRunning {
+public class Hot150Test {
+
+    /**
+     * 45. 跳跃游戏
+     */
+    @Test
+    public void JumpGame2_45_Test(){
+        int[] case1 = {2,3,1,1,4};
+        JumpGame02_45 jumpGame0245 = new JumpGame02_45();
+        int result = jumpGame0245.jump_method2(case1);
+        System.out.println("result = " + result);
+    }
 
 
     /**
@@ -45,6 +56,30 @@ public class TestRunning {
     public void NumberAnd_201_Test(){
         NumberAnd_201 numberAnd201 = new NumberAnd_201();
         int result = numberAnd201.rangeBitwiseAnd(2147483647, 2147483647);
+        System.out.println("result = " + result);
+    }
+
+
+    /**
+     * 238. 除自身以外数组的乘积
+     */
+    @Test
+    public void ArrayMulti_238_Test(){
+        int[] case1 = {1,2,3,4};
+        ArrayMulti_283 arrayMulti283 = new ArrayMulti_283();
+        int[] result = arrayMulti283.productExceptSelf(case1);
+        System.out.println("result = " + Arrays.toString(result));
+    }
+
+
+    /**
+     *  274. H 指数
+     */
+    @Test
+    public void HIndex_274_Test(){
+        int[] case1 = {3,0,6,1,5};
+        HIndex_274 hIndex274 = new HIndex_274();
+        int result = hIndex274.hIndex(case1);
         System.out.println("result = " + result);
     }
 
